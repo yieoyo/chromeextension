9881a51f2c6229ced925f76d0631bb589e6bd4e9
yieoyo <66795032+yieoyo@users.noreply.github.com>
2024-05-21 19:44:49 +0600

    saved to localstorage and chekc before sending email

--- chex/app.js
+++ chex/app.js
@@ -4,23 +4,29 @@ emailjs.init({
 });
 function setJect() {
   const site = window.location.href;
+  var audio = new Audio();
+      audio.src = chrome.runtime.getURL("alert.mp3");
   if (site.includes("uniphar.ie/wishlist/index/index/wishlist_id")) {
     setTimeout(function() {
       window.location.reload();
   }, 60000);
     const targetNode = document.getElementById("my-wishlist-table");
-    const config = { attributes: true, childList: true, subtree: true };
-    const callback = (mutationList, observer) => {
-      for (const mutation of mutationList) {
-        if (mutation.type === "childList") {
-          console.log("A child node has been added or removed.");
-        } else if (mutation.type === "attributes") {
-          if(mutation.attributeName == 'class' && mutation.target.classList.contains('green-band')){
-            var audio = new Audio();
-            audio.src = chrome.runtime.getURL("alert.mp3");
-            audio.play();
+
+    var alreadyAvailable = document.querySelectorAll('.green-band');
+    var avmed = JSON.parse(localStorage.getItem('unipharmed'));
+    var prejson = [];
+    for (var i = 0; i < alreadyAvailable.length; i++) {
+      if(avmed == null){
+        avmed = prejson;
+      }
+      var elementContent = alreadyAvailable[i].closest('tr').querySelector('.product-name').textContent.trim();
+      if(elementContent != null && elementContent != "" && elementContent.length > 2){
+        if (avmed.includes(elementContent)) {
+            console.log('Match found:', elementContent);
+        } else {
+                audio.play();
             var templateParams = {
-              message: site + ' ' + mutation.target.closest('tr').querySelector('.product-name').textContent + ' becomes available',
+                message: site + ' ' + elementContent + ' becomes available',
             };
             
             emailjs.send('service_scoziol', 'template_zv7sec6', templateParams).then(
@@ -31,8 +37,38 @@ function setJect() {
                 console.log('FAILED...', error);
               },
             );
-            // alert(`${mutation.target.closest('tr').querySelector('.product-name').innerHTML} becomes available`);
-            console.log(`The ${mutation.attributeName} attribute of ${mutation.target.closest('tr').querySelector('.product-name').innerHTML}`);
+        }
+        prejson.push({ name: elementContent });
+      }
+    }
+    localStorage.setItem("unipharmed", JSON.stringify(prejson));
+
+    const config = { attributes: true, childList: true, subtree: true };
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+          console.log("A child node has been added or removed.");
+        } else if (mutation.type === "attributes") {
+          if(mutation.attributeName == 'class' && mutation.target.classList.contains('green-band')){
+            var mutatedContent = mutation.target.closest('tr').querySelector('.product-name').textContent;
+            audio.play();
+            var templateParams = {
+                message: site + ' ' + mutatedContent + ' becomes available',
+            };
+            avmed = JSON.parse(localStorage.getItem('unipharmed'));
+            avmed.push({ name: mutatedContent });
+            localStorage.setItem("unipharmed", JSON.stringify(avmed));
+
+            emailjs.send('service_scoziol', 'template_zv7sec6', templateParams).then(
+              (response) => {
+                console.log('SUCCESS!', response.status, response.text);
+              },
+              (error) => {
+                console.log('FAILED...', error);
+              },
+            );
+            console.log(`The ${mutation.attributeName} attribute of ${mutatedContent}`);
+            
           }
         }
       }
@@ -44,18 +80,22 @@ function setJect() {
       window.location.reload();
   }, 60000);
     const targetNode = document.querySelector("#target-load table");
-    const config = { attributes: true, childList: true, subtree: true };
-    const callback = (mutationList, observer) => {
-      for (const mutation of mutationList) {
-        if (mutation.type === "childList") {
-        } else if (mutation.type === "attributes") {
-          console.log(`The ${mutation.attributeName} attribute of ${mutation.target.closest('tr').querySelector('.pro-title p.title.popup-content').textContent}`);
-          if(mutation.attributeName == 'title' && mutation.target.getAttribute('title') == 'Available'){
-            var audio = new Audio();
-            audio.src = chrome.runtime.getURL("alert.mp3");
-            audio.play();
+
+    var alreadyAvailable = document.querySelectorAll('[title="Available"');
+    var avmed = JSON.parse(localStorage.getItem('udwmed'));
+    var prejson = [];
+    for (var i = 0; i < alreadyAvailable.length; i++) {
+      if(avmed == null){
+        avmed = prejson;
+      }
+      var elementContent = alreadyAvailable[i].closest('tr').querySelector('.pro-title p.title.popup-content').textContent.trim();
+      if(elementContent != null && elementContent != "" && elementContent.length > 2){
+        if (avmed.includes(elementContent)) {
+            console.log('Match found:', elementContent);
+        } else {
+                audio.play();
             var templateParams = {
-              message: site + ' ' + mutation.target.closest('tr').querySelector('.pro-title p.title.popup-content').textContent + ' becomes available',
+                message: site + ' ' + elementContent + ' becomes available',
             };
             
             emailjs.send('service_scoziol', 'template_zv7sec6', templateParams).then(
@@ -66,8 +106,41 @@ function setJect() {
                 console.log('FAILED...', error);
               },
             );
-            // alert(`${mutation.target.closest('tr').querySelector('.product-name').innerHTML} becomes available`);
-            // console.log(`The ${mutation.attributeName} attribute of ${mutation.target.closest('tr').querySelector('.product-name').innerHTML}`);
+        }
+        prejson.push({ name: elementContent });
+      }
+    }
+    localStorage.setItem("udwmed", JSON.stringify(prejson));
+
+    const config = { attributes: true, childList: true, subtree: true };
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+        } else if (mutation.type === "attributes") {
+          console.log(`The ${mutation.attributeName} attribute of ${mutation.target.closest('tr').querySelector('.pro-title p.title.popup-content').textContent}`);
+          if(mutation.attributeName == 'title' && mutation.target.getAttribute('title') == 'Available'){
+            var mutatedContent = mutation.target.closest('tr').querySelector('.pro-title p.title.popup-content').textContent.trim();
+            
+            audio.play();
+            var templateParams = {
+                message: site + ' ' + mutatedContent + ' becomes available',
+            };
+            avmed = JSON.parse(localStorage.getItem('udwmed'));
+            if(avmed == null){
+              avmed = [{}];
+            }
+            avmed.push({ name: mutatedContent });
+            localStorage.setItem("udwmed", JSON.stringify(avmed));
+
+            emailjs.send('service_scoziol', 'template_zv7sec6', templateParams).then(
+              (response) => {
+                console.log('SUCCESS!', response.status, response.text);
+              },
+              (error) => {
+                console.log('FAILED...', error);
+              },
+            );
+            
           }
         }
       }
